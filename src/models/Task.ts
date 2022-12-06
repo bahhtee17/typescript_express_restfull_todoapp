ec49cd0f5847798711f3c0c5bf7cb518aa46df29
Baxtiyor <baxtiyorshukurjanov36@gmail.com>
2022-12-06 22:09:57 +0200

    added task model

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,1 +1,18 @@
 import mongoose, { Schema, Document } from 'mongoose';
+
+export interface ITask {
+    title: string;
+    description: string;
+}
+
+export interface ITaskModel extends ITask, Document {}
+
+const TaskSchema: Schema = new Schema(
+    {
+        title: { type: String, required: true },
+        description: { type: String, required: true }
+    },
+    { timestamps: true }
+);
+
+export default mongoose.model<ITaskModel>('Task', TaskSchema);
